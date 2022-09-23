217e50c8b13f56005b8ca273bf79347611f06b32
OLINzin <106200669+OLINzin@users.noreply.github.com>
2022-09-22 23:32:36 -0300

    Função dos botões (Gabriel) 22 de setembro 23:32

--- app/src/main/java/com/olin/homegame/CadastroActivity.java
+++ app/src/main/java/com/olin/homegame/CadastroActivity.java
@@ -2,13 +2,34 @@ package com.olin.homegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        IniciarComponentes();
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(CadastroActivity.this,Cadastro2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void IniciarComponentes(){
+        button2 = findViewById(R.id.button2);
     }
 }
